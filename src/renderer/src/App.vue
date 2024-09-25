2f482d46f200459b9566093bb132a40caf136b59
gukazma <gukazma@proton.me>
2024-09-25 18:03:10 +0800

    ✨ feat: 成功加载了

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,24 +1,31 @@
 <script setup>
 import Versions from './components/Versions.vue'
-
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
 </script>
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
+  <div class="home viewer">
+    <vc-viewer @ready="onViewerReady">
+      <vc-entity
+        ref="entity"
+        :billboard="billboard"
+        :position="{ lng: 108, lat: 32 }"
+        :point="point"
+        :label="label"
+        @click="onEntityEvt"
+        @mouseover="onEntityEvt"
+        @mouseout="onEntityEvt"
+      >
+        <vc-graphics-rectangle
+          :coordinates="[130, 20, 80, 25]"
+          material="green"
+        ></vc-graphics-rectangle>
+      </vc-entity>
+      <vc-layer-imagery>
+        <vc-imagery-provider-osm></vc-imagery-provider-osm>
+      </vc-layer-imagery>
+      <vc-navigation></vc-navigation>
+      <vc-measurements :offset="[0, 40]" :mainFabOpts="mainFabOpts"></vc-measurements>
+    </vc-viewer>
   </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
 </template>
--- src/renderer/src/main.js
+++ src/renderer/src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
-
+// main.ts
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueCesium from 'vue-cesium'
+import 'vue-cesium/dist/index.css'
+
+createApp(App).use(VueCesium, {
+    cesiumPath: 'https://unpkg.com/cesium/Build/Cesium/Cesium.js'
+  }).mount('#app')
